c50237160c083e70643843a5e7ff39c72a78f00e
bryancheong96 <bryancheong96@users.noreply.github.com>
2020-03-26 20:06:01 +0000

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TemPloy.Views;
+using Xamarin.Forms;
+
+namespace TemPloy
+{
+	public partial class App : Application
+	{
+		string version = "Jobseeker";
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new LoginPage(version);
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
